620d46d24f4aa3f5c703deb2d4dd4e967094d6db
mehedihasan1122 <mehedirayhan1122@gmail.com>
2021-03-02 02:39:50 +0600

    first update

---
+++ Bnak_assignment1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bnak_assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acod = new Account();
+            Bank bobj = new Bank();
+            acod.GetAddress("230", "nikunja", "dhaka", "BD");
+            bobj.AccountName = "mehedi hasan";
+            bobj.AccountNumber = "02";
+            bobj.Balance = 500000d;
+            bobj.Deposit(500);
+            bobj.Withdraw(50);
+            acod.ShowAccountInformation();
+
+            bobj.BankName = "Shonali";
+            bobj.PrintAccountDetails();
+        }
+    }
+}
