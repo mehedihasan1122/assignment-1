4ab5a6e01b6787b926f140bda32127eec70c6c68
mehedihasan1122 <mehedirayhan1122@gmail.com>
2021-03-02 22:41:09 +0600

    3rd update

--- Bnak_assignment1/Account.cs
+++ Bnak_assignment1/Account.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace Bnak_assignment1
 {
-    class Account
+    class Account : Address
     {
+        private string accountNumber;
+        private string accountName;
+        private double balance;
+        private Address address;
+
+        public string AccountNumber
+        { get; set; }
+        public string AccountName
+        { get; set; }
+        public double Balance
+        { get; set; }
+        public void Withdraw(double amount)
+        {
+            if (balance <= 0)
+            {
+                Console.WriteLine("Insufficient balance \n Cant not withdraw this money");
+            }
+            else
+            {
+                balance = balance - amount;
+                Console.WriteLine("Withdraw successful \n your corrent balance is: " + balance);
+            }
+        }
+        public void Deposit(double amount)
+        {
+            balance = balance + amount;
+            Console.WriteLine("Deposit successful \n your corrent balance is: " + balance);
+        }
+        public void Transfer(string Acnum, string Acname, double amount)
+        {
+            if (accountName == Acname && accountNumber == Acnum)
+            {
+                if (balance <= 0)
+                {
+                    Console.WriteLine("Insufficient balance \n Cant not Transfer this money");
+                }
+                else
+                {
+                    balance = balance - amount;
+                    Console.WriteLine("Transfer successful \n your corrent balance is: " + balance);
+                }
+            }
+        }
+        public void ShowAccountInformation()
+        {
+            Console.WriteLine("Account Name: " + AccountName + "\nAccount Number: " + AccountNumber + "\nBalance: " + Balance);
+            ShowAddress();
+        }
+
     }
 }
--- Bnak_assignment1/Bank.cs
+++ Bnak_assignment1/Bank.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Bnak_assignment1
 {
-    class Bank
+    class Bank : Account
     {
+        public string BankName
+        {
+            get; set;
+        }
+        public Account[] myBank;
+        public void AddAccount(Account[] account)
+        {
+
+        }
+        public void DeleteAccount(int accountNumber)
+        {
+
+        }
+        public void Transaction()
+        {
+
+        }
+        public void PrintAccountDetails()
+        {
+            ShowAccountInformation();
+            Console.WriteLine("Bank Name: " + BankName);
+        }
     }
 }
