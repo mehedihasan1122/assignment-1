118d288aa38007c45a96e0d0d927581543823e6a
mehedihasan1122 <mehedirayhan1122@gmail.com>
2021-03-03 09:26:44 +0600

    5th update

--- Bnak_assignment1/Bank.cs
+++ Bnak_assignment1/Bank.cs
@@ -9,27 +9,68 @@ namespace Bnak_assignment1
 {
     class Bank : Account
     {
+        public string t;
+        public double Amount { get; set; }
         public string BankName
         {
             get; set;
         }
         public Account[] myBank;
-        public void AddAccount(Account[] account)
-        {
 
-        }
-        public void DeleteAccount(int accountNumber)
+        public int i = 1;
+        public void AddAccount(/*Account[] account*/)
         {
-
+            Console.WriteLine("Enter Your account name: ");
+            AccountName = Console.ReadLine();
+            AccountNumber = Convert.ToString(i);
+            i++;
+            Console.WriteLine("Road No: ");
+            Roadno = Console.ReadLine();
+            Console.WriteLine("House No: ");
+            Houseno = Console.ReadLine();
+            Console.WriteLine("City: ");
+            City = Console.ReadLine();
+            Console.WriteLine("Country: ");
+            Country = Console.ReadLine();
+            
         }
+        
         public void Transaction()
         {
+            Console.WriteLine("Which Transaction do you want? \n");
+            Console.WriteLine("1.Deposit\n2.Withdraw\n3.Transfer");
+            t = Console.ReadLine();
+            switch (t)
+            {
+
+                case "1":
+                    Console.WriteLine("Enter the amount for deposit: ");
+                    Amount = Convert.ToDouble(Console.ReadLine());
+                    Deposit(Amount);
+                    break;
+                case "2":
+                    Console.WriteLine("Enter the amount for Withdraw: ");
+                    Amount = Convert.ToDouble(Console.ReadLine());
+                    Withdraw(Amount);
+                    break;
+                case "3":
+                    Console.WriteLine("You are transfering money.\nEnter Account name: ");
+                    AccountName = Console.ReadLine();
+                    Console.WriteLine("Enter Account number: ");
+                    AccountNumber = Console.ReadLine();
+                    Console.WriteLine("Enter the amount: ");
+                    Amount = Convert.ToDouble(Console.ReadLine());
+                    Transfer(AccountName, AccountName, Amount);
+                    break;
+                default:
+                    Console.WriteLine("Wrong output: ");
+                    break;
+            }
 
         }
         public void PrintAccountDetails()
         {
             ShowAccountInformation();
-            Console.WriteLine("Bank Name: " + BankName);
         }
     }
 }
--- Bnak_assignment1/Program.cs
+++ Bnak_assignment1/Program.cs
@@ -8,20 +8,42 @@ namespace Bnak_assignment1
 {
     class Program
     {
+
+        public string BankName
+        { 
+            get;set;
+        }
         static void Main(string[] args)
         {
-            Account acod = new Account();
+            //Account acod = new Account();
             Bank bobj = new Bank();
-            acod.GetAddress("230", "nikunja", "dhaka", "BD");
-            bobj.AccountName = "mehedi hasan";
-            bobj.AccountNumber = "02";
-            bobj.Balance = 500000d;
-            bobj.Deposit(500);
-            bobj.Withdraw(50);
-            acod.ShowAccountInformation();
+            Console.WriteLine("enter bank name: ");
+            bobj.BankName = Console.ReadLine();
 
-            bobj.BankName = "Agrani";
+            Console.WriteLine("Do you want to creat account or you have account:\n 1.creat   2.Have account");
+            int d1 = Convert.ToInt32(Console.ReadLine());
+            if (d1 == 1)
+            {
+                bobj.AddAccount();
+            }
+            else
+            {
+                Console.WriteLine("You have loged in ");
+
+            }
+            bobj.Transaction();
             bobj.PrintAccountDetails();
+
+
+            //acod.GetAddress("230", "nikunja", "dhaka", "BD");
+           // bobj.AccountName = "mehedi hasan";
+           // bobj.AccountNumber = "02";
+            //bobj.Balance = 500000d;
+           // bobj.Deposit(500);
+           //bobj.Withdraw(50);
+            //acod.ShowAccountInformation();
+            //bobj.BankName = "Agrani";
+           // bobj.PrintAccountDetails();
         }
     }
 }
