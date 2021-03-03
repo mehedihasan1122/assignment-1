02788f52a810d0989e99d505064a86d595f94d74
mehedihasan1122 <mehedirayhan1122@gmail.com>
2021-03-03 09:40:51 +0600

    6th update

--- Bnak_assignment1/Address.cs
+++ Bnak_assignment1/Address.cs
@@ -8,10 +8,10 @@ namespace Bnak_assignment1
 {
     class Address
     {
-        private string roadNo;
-        private string houseNo;
-        private string city;
-        private string country;
+        public string roadNo { get; set; }
+        public string houseNo { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
         public void GetAddress(string r, string h, string c, string con)
         {
             roadNo = r;
